15542696eee7bd05ff264713ce9f6c13b3861e43
Jensen Somers <jensen@jsomers.be>
2020-10-14 15:02:35 +0200

    add delete user

---
+++ src/VerticalSliceArchictureDemo.Web/Features/V1/Users/DeleteUser/DeleteUserHandler.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using VerticalSliceArchictureDemo.Web.Common.Http.Exceptions;
+using VerticalSliceArchictureDemo.Web.Persistence;
+
+namespace VerticalSliceArchictureDemo.Web.Features.V1.Users.DeleteUser
+{
+    public class DeleteUserHandler : IRequestHandler<DeleteUserRequest>
+    {
+        private readonly DemoDbContext _dbContext;
+
+        public DeleteUserHandler(DemoDbContext dbContext)
+            => _dbContext = dbContext;
+
+        public async Task<Unit> Handle(DeleteUserRequest request, CancellationToken cancellationToken)
+        {
+            var entry = await _dbContext.Users
+                .SingleOrDefaultAsync(x => x.Id == request.Id, cancellationToken)
+                .ConfigureAwait(false);
+
+            if (entry == null)
+            {
+                throw new HttpNotFoundException();
+            }
+
+            _dbContext.Users.Remove(entry);
+
+            await _dbContext.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
+
+            return Unit.Value;
+        }
+    }
+}
---
+++ src/VerticalSliceArchictureDemo.Web/Features/V1/Users/DeleteUser/DeleteUserRequest.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using Newtonsoft.Json;
+
+namespace VerticalSliceArchictureDemo.Web.Features.V1.Users.DeleteUser
+{
+    public class DeleteUserRequest : IRequest
+    {
+        [JsonIgnore]
+        public int Id { get; set; }
+    }
+}
